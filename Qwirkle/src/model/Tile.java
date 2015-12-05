0215af3056d94e8c6954d890a53c41e060dcd85f
Anne Steenbeek <annesteenbeek@gmail.com>
2015-12-05 17:35:41 +0100

    Added toString method in enums

--- Qwirkle/src/model/Tile.java
+++ Qwirkle/src/model/Tile.java
@@ -7,32 +7,65 @@ public class Tile {
     
 	//Square, Circle, Star, Diamond, Flower, Jew, Empty
 	public enum Shape {
-		$, O, X, ƒ, Ø, ¥,
+		$ ("$"),
+		O ("O"),
+		X ("X"),
+		ƒ ("ƒ"),
+		Ø ("Ø"),
+		¥ ("¥");
+
+		private final String value;
+		
+		private Shape(final String value) {
+			this.value = value;
+		}
+
+		// method to return String from enum type
+		public String toString() {
+			return value;
+		}	
 	}
 	
 	//Red, Blue, Green, Yellow, Orange, Purple
-	public enum Colour {
-		R, B, G, Y, O, P
+	public enum Color {
+		R ("red"),
+		B ("blue"),
+		G ("green"),
+		Y ("yellow"), 
+		O ("orange"),
+		P ("purple");
+		
+		private final String value;
+		
+		private Color(final String value) {
+			this.value = value;
+		}
+		
+		/* Method to return String from enum
+		 * @return string of color
+		 */
+		public String toString() {
+			return value;
+		}		
 	}
-	
     private final Shape shape;
-    private final Colour colour;
+    private final Color color;
     
-    Tile(Shape shape, Colour colour) {
+    Tile(Shape shape, Color color) {
         this.shape = shape;
-        this.colour = colour;
+        this.color = color;
     }
 
     public Shape getShape() { 
     	return shape; 
     }
     
-    public Colour getColour() { 
-    	return colour; 
+    public Color getColor() { 
+    	return color; 
     }
     
     public String toString() { 
-    	return shape + " " + colour; 
+    	return shape + " " + color; 
     }
 }
 	
