3cb8643a88c0dd0317a8ba4dba1f8d5939036c1d
annesteenbeek <annesteenbeek@gmail.com>
2016-01-29 18:02:06 +0100

    Moved Qwirkle

---
+++ Qwirkle/src/controller/Qwirkle.java
@@ -0,0 +1,37 @@
+package controller;
+
+import controller.*;
+import view.Game;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Qwirkle {
+	public static void main(String[] a) {
+		String[] args = new String[2];
+		args[0] = "-S";
+		args[1] = "-N";
+    	if (args.length > 1 && args.length <= 4) {
+    		Player[] players = new Player[args.length];
+    		for (int i = 0; i < args.length; i++) {
+				String autoname = "Fred" + Integer.toString(i);
+    			switch (args[i]) {
+    				case "-N":
+						players[i] = new ComputerPlayer(new NaiveStrategy(), autoname);
+						break;
+    				case "-S":
+						players[i] = new ComputerPlayer(new SmartStrategy(), autoname);
+    					break;
+    				default:
+    					players[i] = new HumanPlayer(args[i]);
+    					break;
+    			}
+    		}
+
+    		Game game = new Game(new ArrayList<Player>(Arrays.asList(players)));
+    		game.start();
+    	} else {
+    		System.err.println("Wrong input arguments");
+    	}
+	}
+}
